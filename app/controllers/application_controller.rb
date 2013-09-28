e04628c3a6b9d95769df900b3b35439e834ed991
Xavier Denis <xldenis@gmail.com>
2013-09-28 01:45:11 -0400

    Add helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  helper_method: logged_in?, current_user
+
+  def current_user
+    @current_user ||= User.find(session[:current_user_id])
+  end
+  def logged_in?
+    @current_user != nil
+  end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
     def create
         @user = User.find_or_create_by_hash(request.env['omniauth.auth'])
-        session[:current_user] = @user
+        session[:current_user_id] = @user.id
         redirect_to :root
     end
 end
