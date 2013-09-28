c16d80651e3c10523158992132e8222eb87e43db
C-A de Salaberry <cadesalaberry@yahoo.com>
2013-09-28 06:09:05 -0400

    Fix: Redis and Sidekiq

--- app/workers/facebook_worker.rb
+++ app/workers/facebook_worker.rb
@@ -2,16 +2,14 @@ class FacebookWorker
   include Sidekiq::Worker
 
   def perform(source_id)
-    source = source.find(source_id)
+    source = Source.find(source_id)
 
     graph = Koala::Facebook::API.new(source.token)
     posts = graph.get_connections("me","feed")
     posts.each do |post|
       source.posts.create(post_id:post['id'],
                           type:post['type'],
-                          message['test']:post['message'],
-                          message['link']:post['link'],
-                          link:post['link'],
+                          message: {text: post['message'],link:post['link']},
                           created_time:post['created_time'])
     end
 
