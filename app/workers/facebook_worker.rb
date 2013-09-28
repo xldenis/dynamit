8382006e1410707e5056e533c8c14d89d9d0a0af
Xavier Denis <xldenis@gmail.com>
2013-09-28 06:34:39 -0400

    Fix workers

--- app/workers/facebook_worker.rb
+++ app/workers/facebook_worker.rb
@@ -1,17 +1,20 @@
 class FacebookWorker
   include Sidekiq::Worker
 
+  # def perform(source_id)
+  #   source = Source.find(source_id)
+
+  #   graph = Koala::Facebook::API.new(source.token)
+  #   posts = graph.get_connections("me","feed")
+  #   posts.each do |post|
+  #     source.posts.create(post_id:post['id'],
+  #                         type:post['type'],
+  #                         message: {text: post['message'],link:post['link']},
+  #                         created_time:post['created_time'])
+  #   end
+
+  # end
   def perform(source_id)
-    source = Source.find(source_id)
-
-    graph = Koala::Facebook::API.new(source.token)
-    posts = graph.get_connections("me","feed")
-    posts.each do |post|
-      source.posts.create(post_id:post['id'],
-                          type:post['type'],
-                          message: {text: post['message'],link:post['link']},
-                          created_time:post['created_time'])
-    end
-
+    Source.find(source_id)
   end
 end
