08555827b3c80cbbb26506fa9d9694174ca4bce9
Graham Ludwinski <graham.ludwinski@gmail.com>
2013-09-28 04:34:49 -0400

    styled the front end a bit

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,8 @@
 class WelcomeController < ApplicationController
   def index
+  respond_to do |format|
+  	format.html {"index.json"}
+  	format.json {"index.json"}
+  	end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Dynamite::Application.routes.draw do
   get '/auth/:provider/callback',to: 'sessions#create'
   root :to => "feeds#index"
+  get '/welcome/',to: 'welcome#index'
 end
