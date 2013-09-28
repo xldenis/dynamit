f949b222c1f48965f96753c5f6be1b72e6977b7c
Xavier Denis <xldenis@gmail.com>
2013-09-28 02:39:30 -0400

    Add api routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Dynamite::Application.routes.draw do
   get '/auth/:provider/callback',to: 'sessions#create'
   root :to => "feeds#index"
+  
+  shallow do
+    namespace :api,:provides => :json do
+      resources :users do
+        resources :sources do
+          resources :posts
+        end
+      end
+    end
+  end
+
 end
