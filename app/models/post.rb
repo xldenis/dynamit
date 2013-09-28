ee72b0a7ddac2e4c0ce4dcf1dc3b43f8d7d3970a
C-A de Salaberry <cadesalaberry@yahoo.com>
2013-09-28 03:54:33 -0400

    Commenting the post model.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post
   include Mongoid::Document
   embedded_in :source
+  
   field :create_time, :descriptor => DateTime
 
   # Holds the content type for a Facebook post, or the title of a RSS post
@@ -8,9 +9,14 @@ class Post
 
   # Holds the 'text' content of the post
   # (and the link of the attachement if there is one on facebook)
-  field :message, type:Hash 
+  field :message, type:Hash
+
+  # Holds the link to acces directly to the post
   field :link
+
+  # Holds the Facebook post_id or the etag of the RSS feed
   field :post_id
+
   field :author
 
   validates_presence_of :post_id
